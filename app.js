fb3d3d279317c258e975dd19a4f33a83f24d2485
George Albert Palma Silva Junior <george.junior@mazars.com.br>
2025-09-15 13:58:44 -0300

    Salvar4

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let participante = document.querySelector('input');
+let participante = document.querySelector('input ');
 let amigosParticipantes = [];
 let adicionarBtn = document.querySelector('#adicionar');
 let sortearBtn = document.querySelector('#sortear');
 let ExibirAmigoSorteado = document.querySelector('listaAmigos');
-let jaSorteado = false;
+
 function limparInput(){
     participante.value = '';
 }
@@ -36,19 +36,7 @@ function sortearAmigo(){
     exibirAmigoSorteado(amigoSorteado);
     let mudarBtn = document.querySelector('#botao-sortear');
     mudarBtn.innerHTML = 'Refazer lista';
-    if(!jaSorteado){
-        jaSorteado = true;
-        return;
-    }
-    btnSortear.addEventListener('click', () => { 
-        if (amigosParticipantes.length > 0) {
-            amigosParticipantes = [];
-            exibirAmigoSorteado('Lista refeita! Adicione novos amigos.');
-            mudarBtn.innerHTML = 'Sortear amigo';
-            }
-        return;
-      }
-    
+
 };
 function exibirAmigoSorteado(amigoSorteado){
     let exibirAmigoSorteado = document.querySelector('h2');
