3072dad239db0ec2c059271ab714cc0b54a23a90
George Albert Palma Silva Junior <george.junior@mazars.com.br>
2025-09-15 12:16:22 -0300

    Salvar

--- app.js
+++ app.js
@@ -1,2 +1,53 @@
-let nome = [];
-let id = "teste";
+let participante = document.querySelector('input ');
+let amigosParticipantes = [];
+let adicionarBtn = document.querySelector('#adicionar');
+let sortearBtn = document.querySelector('#sortear');
+let ExibirAmigoSorteado = document.querySelector('listaAmigos');
+
+function limparInput(){
+    participante.value = '';
+}
+function validarInput(){
+    if(participante.value === ''){
+        exibirAmigoSorteado('Por favor, insira um nome válido.');
+        return false;
+    }
+    if(amigosParticipantes.includes(participante.value)){
+        exibirAmigoSorteado('Esse nome já foi adicionado. Por favor, insira um nome diferente.');
+        return false;
+    }
+}
+function adicionarAmigo(){
+    let valido = validarInput();
+    if(valido === false){
+        limparInput();
+        return; 
+    }
+    amigosParticipantes.push(participante.value);
+    console.log('Adicionando amigo...');
+    console.log(amigosParticipantes);
+    limparInput();
+    
+};
+function sortearAmigo(){
+    let numeroSorteado = Math.floor(Math.random() * amigosParticipantes.length);
+    let amigoSorteado = amigosParticipantes[numeroSorteado];
+    console.log('Amigo sorteado: ' + amigoSorteado);
+    exibirAmigoSorteado(amigoSorteado);
+    let mudarBtn = document.querySelector('#botao-sortear');
+    mudarBtn.innerHTML = 'Sortear novamente';
+
+};
+function exibirAmigoSorteado(amigoSorteado){
+    let exibirAmigoSorteado = document.querySelector('h2');
+    exibirAmigoSorteado.innerHTML = 'Amigo sorteado foi: ' + amigoSorteado;
+}
+
+if(adicionarBtn){
+    adicionarBtn.addEventListener('click', adicionarAmigo);
+    participante.value = '';
+}
+if(sortearBtn){
+    sortearBtn.addEventListener('click', sortearAmigo);
+}
+
