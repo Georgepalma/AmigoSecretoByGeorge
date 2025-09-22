b1a1cd3f384938e13d1e294e5a0159e096868bd6
Georgepalma <georgepalma600@gmail.com>
2025-09-21 23:56:02 -0300

    Final

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+//Selecionando elementos do DOC
 let participante = document.querySelector('input ');
+//Array para armazenar os amigos participantes
 let amigosParticipantes = [];
+//Selecionando botões do DOC
 let adicionarBtn = document.querySelector('#adicionar');
 let sortearBtn = document.querySelector('#sortear');
 let ExibirAmigoSorteado = document.querySelector('listaAmigos');
-
+//Função para limpar o input do usuário
 function limparInput(){
     participante.value = '';
 }
+//Função para validar o input do usuário
 function validarInput(){
     if(participante.value === ''){
         exibirAmigoSorteado('Por favor, insira um nome válido.');
@@ -17,6 +21,7 @@ function validarInput(){
         return false;
     }
 }
+//Função para adicionar amigo na lista
 function adicionarAmigo(){
     let valido = validarInput();
     if(valido === false){
@@ -29,25 +34,24 @@ function adicionarAmigo(){
     limparInput();
     
 };
+//Função para limpar a lista de amigos
+function limparLista(){
+    amigosParticipantes = [];
+    console.log('Lista limpa com sucesso!');
+}
+//Função para sortear amigo, limpar input e lista.
 function sortearAmigo(){
     let numeroSorteado = Math.floor(Math.random() * amigosParticipantes.length);
     let amigoSorteado = amigosParticipantes[numeroSorteado];
     console.log('Amigo sorteado: ' + amigoSorteado);
     exibirAmigoSorteado(amigoSorteado);
-    let mudarBtn = document.querySelector('#botao-sortear');
-    mudarBtn.innerHTML = 'Refazer lista';
+
+    limparInput();
+    limparLista();
 
 };
+//Função para exibir o amigo sorteado na tela
 function exibirAmigoSorteado(amigoSorteado){
     let exibirAmigoSorteado = document.querySelector('h2');
     exibirAmigoSorteado.innerHTML = 'Amigo sorteado foi: ' + amigoSorteado;
 }
-
-if(adicionarBtn){
-    adicionarBtn.addEventListener('click', adicionarAmigo);
-    participante.value = '';
-}
-if(sortearBtn){
-    sortearBtn.addEventListener('click', sortearAmigo);
-}
-
